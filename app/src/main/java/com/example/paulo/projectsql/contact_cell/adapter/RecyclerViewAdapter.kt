361de1de15183a54956c07d6f0c05bc1a8d4e86a
Paulo Felipe <p.felipe@msn.com>
2018-07-03 09:02:40 -0300

    Implementing RecyclerView with MVVM

---
+++ app/src/main/java/com/example/paulo/projectsql/contact_cell/adapter/RecyclerViewAdapter.kt
@@ -0,0 +1,2 @@
+package com.example.paulo.projectsql.contact_cell.adapter
+
--- app/src/main/java/com/example/paulo/projectsql/contact_cell/adapter/ViewHolder.kt
+++ app/src/main/java/com/example/paulo/projectsql/contact_cell/adapter/ViewHolder.kt
@@ -1,8 +1,16 @@
 package com.example.paulo.projectsql.contact_cell.adapter
 
+import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
+import com.example.paulo.projectsql.BR
 
 
-class ViewHolder(): RecyclerView.ViewHolder() {
+class ViewHolder(val itemDataBinding: ViewDataBinding): RecyclerView.ViewHolder(itemDataBinding.root) {
 
+
+    fun bind(model: ItemViewModel){
+        itemDataBinding?.setVariable(BR.viewModel, model)
+    }
+
+    fun getViewDataBinding(): ViewDataBinding = itemDataBinding
 }
